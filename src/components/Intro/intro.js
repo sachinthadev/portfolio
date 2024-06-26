438a61b8a0e1a4c4dc64982e71192d013d07cf61
sachinthadev <sahansachintha.dev@gmail.com>
2024-06-27 00:47:43 +0530

    chage hire me logo

--- src/components/Intro/intro.js
+++ src/components/Intro/intro.js
@@ -2,17 +2,16 @@ import React from 'react';
 import './intro.css';
 import bg from '../../assets/image.png';
 import { Link } from 'react-scroll';
-import btnImg from '../../assets/hireme.png';
+import btnImg from '../../assets/briefcase.png';
 
 const Intro = () => {
   return (
-   <section id="Intro">
-    <div className='introContent'>
-        <span className='hello'>Hello,</span>
-        <span className='introText'>I'm <span className='introName'>Sachintha</span><br/>Software Engineer</span>
-        <p className='introPara'>I am a skilled full-stack software developer with extensive experience in creating comprehensive software solutions. </p>
-        <Link><button className='btn'><img src={btnImg} alt='Hire Me'/>Hire Me</button></Link>
-
+   <section id="intro">
+    <div className="introContent">
+        <span className="hello">Hello,</span>
+        <span className="introText">I'm <span className='introName'>Sachintha</span><br/>Software Engineer</span>
+        <p className="introPara">I am a skilled full-stack software developer with extensive experience in creating <br/>comprehensive software solutions. </p>
+        <Link><button className="btn"><img src={btnImg} alt='Hire Me'className='btnImg'/>Hire Me</button></Link>
     </div>
     <img src={bg} alt="portfolio" className="bg" />
    </section>
