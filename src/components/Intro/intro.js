8e9563aa6c7019fd83bd71087012f7ffdf6534ce
sachinthadev <sahansachintha.dev@gmail.com>
2024-07-16 22:10:59 +0530

    update intro.js using react-simple-typewriter

--- src/components/Intro/intro.js
+++ src/components/Intro/intro.js
@@ -3,13 +3,20 @@ import './intro.css';
 import bg from '../../assets/image.png';
 import { Link } from 'react-scroll';
 import btnImg from '../../assets/briefcase.png';
+import {useTypewriter,Cursor} from 'react-simple-typewriter';
 
 const Intro = () => {
+  const [text] = useTypewriter({
+    words: ['Sahan','Sachintha','Athapaththu'],
+    loop: 0
+  })
+  
+
   return (
    <section id='intro'>
     <div className="introContent">
         <span className="hello">Hello,</span>
-        <span className="introText">I'm <span className='introName'>Sachintha</span><br/>Software Engineer</span>
+        <span className="introText">I'm <span className='introName'>{text}<Cursor/></span><br/>Software Developer</span>
         <p className="introPara">I am a skilled full-stack software developer with extensive experience in creating <br/>comprehensive software solutions. </p>
         <Link><button className="btn"><img src={btnImg} alt='Hire'className="btnImg"/>Hire Me</button></Link>
     </div>
